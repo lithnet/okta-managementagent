34e44f2c846af898af8426c24802e23224916009
ryannewington <ryan@lithiumblue.com>
2018-05-20 20:19:59 +1000

    Adds packaged MA

--- src/Lithnet.Okta.ManagementAgent/Properties/AssemblyInfo.cs
+++ src/Lithnet.Okta.ManagementAgent/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Lithnet.Okta.ManagementAgent")]
+[assembly: AssemblyTitle("Lithnet Okta Management Agent for Microsoft Identity Manager")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Lithnet.Okta.ManagementAgent")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCompany("Lithnet")]
+[assembly: AssemblyProduct("Lithnet Okta Management Agent for Microsoft Identity Manager")]
+[assembly: AssemblyCopyright("Copyright © Lithnet 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,4 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
